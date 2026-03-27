f1aec01eaed49b499c07f861350dfea87a36cd15
santinopesce00 <spesce@mail.austral.edu.ar>
2026-03-27 11:38:13 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -18,4 +18,12 @@ def grades():
         Para la entrada "3", la salida esperada es:
         Insuficiente
     """
-    pass
+    nota = float(input())
+    if 9 <= nota <= 10:
+        print("Excelente")
+    elif 7<= nota <=8:
+        print("Bueno")
+    elif 5<=nota <=6:
+        print("Regular")
+    elif 0<= nota <=4:
+        print("Insuficiente")
