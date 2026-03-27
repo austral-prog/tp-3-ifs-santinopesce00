6223b4c787fd938534c8f6feaee4f3d2b0549643
santinopesce00 <spesce@mail.austral.edu.ar>
2026-03-27 12:39:45 -0300

    Update exercise_triangle.py

--- exercise_triangle.py
+++ exercise_triangle.py
@@ -15,4 +15,10 @@ def triangle():
         Para las entradas "1", "2" y "5", la salida esperada es:
         Los lados no forman un triangulo valido
     """
-    pass
+    numero1 = float(input())
+    numero2 = float(input())
+    numero3 = float(input())
+    if (numero1 + numero2) > numero3:
+        print("Los lados forman un triangulo valido")
+    else:
+        print("Los lados no forman un triangulo valido")
