90f9c1a0f25d6395526905c2491fd9c7e5d418b2
santinopesce00 <spesce@mail.austral.edu.ar>
2026-03-27 10:42:34 -0300

    Update exercise_positive.py

--- exercise_positive.py
+++ exercise_positive.py
@@ -15,4 +15,10 @@ def positive():
         Para la entrada "0", la salida esperada es:
         El numero es cero
     """
-    pass
+    numero = int(input("Ingresa un numero: "))
+    if numero > 0:
+        print("El numero es positivo")
+    elif numero < 0:
+        print("El numero es negativo")
+    elif numero == 0:
+        print("El numero es cero")
